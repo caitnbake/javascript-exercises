f166cc883e48d3eeca146ec504aacddeacf49d48
caitnbake <caitlin.atterberry@gmail.com>
2024-11-02 14:38:37 -0700

    Update to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let answer = "";
+    if (num < 0) {
+        answer = "ERROR";
+    } else {
+        for (i = 1; i <=num; i++) {
+            answer += string;
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
