96d1b27a259b7fbbf6c711c49bb2e4b0c580cbd6
caitnbake <caitlin.atterberry@gmail.com>
2024-11-02 20:07:15 -0700

    Update findTheOldest to work for people who are still alive

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    let thisYear = new Date().getFullYear();
+    
+    for (let person of people) {
+        let currentAge = (person.yearOfDeath || thisYear) - person.yearOfBirth;
+        let oldestAge = (oldest.yearOfDeath || thisYear) - oldest.yearOfBirth;
+        
+        if (currentAge > oldestAge) {
+            oldest = person;
+        }
+    }
+    return oldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
