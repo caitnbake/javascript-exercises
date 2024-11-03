969a6c50d6c7d629b23ce11488839be1dbff62b2
caitnbake <caitlin.atterberry@gmail.com>
2024-11-02 18:40:01 -0700

    Add cleanInput and reverseInput functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (original) {
+    const cleaned = cleanInput(original);
+    const reverse = reverseInput(cleaned);
+    
+    if (cleaned === reverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+const cleanInput = (original) => {
+    return original.replace(/\W/g, "");
+}
+
+const reverseInput = (cleaned) => {
+    return cleaned.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
