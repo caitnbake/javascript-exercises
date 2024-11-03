8174257b4d9f83b9c40ad73684ec3e91877b2e0c
caitnbake <caitlin.atterberry@gmail.com>
2024-11-02 18:01:18 -0700

    Refactor for readability

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,9 @@
-const convertToCelsius = function(temp) {
-  let toCelsius = (temp - 32) * (5 / 9);
-  return Math.round(toCelsius * 10) / 10;
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function(temp) {
-  let toFahrenheit = temp * (9 / 5) + 32;
-  return Math.round(toFahrenheit * 10) / 10;
+const convertToFahrenheit = function(celsius) {
+  return Math.round((celsius * ( 9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
