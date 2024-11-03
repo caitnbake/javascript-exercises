03b90992e849ea6a436f4d53eef97d999285e54f
caitnbake <caitlin.atterberry@gmail.com>
2024-11-02 19:37:27 -0700

    Refactor for cleaner code

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,5 @@
 const getTheTitles = function(books) {
-    let titles = [];
-    for (let book of books) {
-        titles.push(book.title);
-    }
-    return titles;
+    return books.map((book) => book.title);
 };
 
 // Do not edit below this line
